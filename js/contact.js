e3bcde600bb3fcae0b38a3e4b6d3683962ab7727
cherka02 <69398852+cherka02@users.noreply.github.com>
2021-12-15 20:07:25 +0100

    JavaScript

---
+++ js/contact.js
@@ -0,0 +1,31 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelector("#send").disabled = true;
+    document.querySelector("#first_name").value = '';
+    document.querySelector("#last_name").value = '';
+    document.querySelector("#email").value = '';
+    document.querySelector("#message").value = '';
+
+    document.querySelector('form').onkeyup = checkContactForm;
+    
+})
+
+
+function checkContactForm(){
+    var first_name = document.querySelector("#first_name").value.trim();
+    var last_name = document.querySelector("#last_name").value.trim();
+    var email = document.querySelector("#email").value.trim();
+    var message = document.querySelector("#message").value.trim();
+    let button = document.querySelector("#send");
+
+    if(first_name.length === 0 || last_name.length === 0
+    || email.length === 0 || message.length === 0){
+        document.getElementById("first_name_alert").style.visibility = "visible";
+        button.disabled = true;        
+    }
+    else {
+        document.getElementById("first_name_alert").style.visibility = "hidden";
+        button.disabled = false;
+    }
+}
