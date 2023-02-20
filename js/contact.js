ca78a0760057e47610f434f00d4e1670e5909c66
cherka02 <69398852+cherka02@users.noreply.github.com>
2023-02-20 21:58:56 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#last_name").value = '';
     document.querySelector("#email").value = '';
     document.querySelector("#message").value = '';
-
+    document.querySelector("#phone").value = '';
     document.querySelector('form').onkeyup = checkContactForm;
     
 })
@@ -17,10 +17,11 @@ function checkContactForm(){
     var last_name = document.querySelector("#last_name").value.trim();
     var email = document.querySelector("#email").value.trim();
     var message = document.querySelector("#message").value.trim();
+    var phone = document.querySelector("#phone").value.trim();
     let button = document.querySelector("#send");
 
     if(first_name.length === 0 || last_name.length === 0
-    || email.length === 0 || message.length === 0){
+    || email.length === 0 || message.length === 0 || phone.length === 0){
         document.getElementById("first_name_alert").style.visibility = "visible";
         button.disabled = true;        
     }
